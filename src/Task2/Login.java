6319c36adaa0c14308fb13881fe169d29e73d685
F223646Ayesha <f223646@cfd.nu.edu.pk>
2024-11-14 12:24:12 +0500

    Added if_condition in Login

--- src/Task2/Login.java
+++ src/Task2/Login.java
@@ -2,9 +2,10 @@ package Task2;
 public class Login {
     public boolean authenticate(String username, String password) {
     	if (username == null || password == null) {
-    	    System.out.println("Invalid input");
+    	    System.out.println("Invalid input, Try again");
     	    return false;
     	}
+    	
     	System.out.println("Authenticating user...");
         return username.equals("admin") && password.equals("password123");
     }
