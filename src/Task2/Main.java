077b8e9750f19e820d5108a43b346c3a1db53bde
Shaheera Malik <shaheeramalik533@gmail.com>
2024-11-14 18:21:00 +0500

    Updated main

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-package Task2;
 public class Main { 
-    public static void main(String[] args) { 
-        System.out.println("Welcome to the application!"); 
-        // greetUser(); // Removed this line to introduce a bug 
-    } 
- 
-    public static void greetUser() { 
-        System.out.println("Hello, User!"); 
-    } 
-=======
-package Task2;
-public class Main { 
-    public static void main(String[] args) { 
-        System.out.println("Welcome to the application!"); 
-        greetUser(); 
-    } 
-    public static void greetUser() { 
-        System.out.println("Hello, User!"); 
-        System.out.println("How are you?"); 
-    } 
->>>>>>> 11c0d3f0c99a92c55b3a4188316d02a70c705dd6
-}
+public static void main(String[] args) { 
+System.out.println("Main branch update."); 
+} 
+} 
